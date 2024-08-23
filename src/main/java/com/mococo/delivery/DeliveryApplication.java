890b5e920fb9362d781eadbe896aa1dcbbfd5df6
dawonseo <oloielo@naver.com>
2024-08-23 14:46:34 +0900

    init commit

---
+++ src/main/java/com/mococo/delivery/DeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.mococo.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryApplication.class, args);
+	}
+
+}
