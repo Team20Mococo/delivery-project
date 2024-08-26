b5714e5530dce0c352a7a76abb89a293d63d5da6
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2024-08-26 15:58:53 +0900

    Revert "Dawon/add store"

--- src/main/java/com/mococo/delivery/adapters/persistence/JpaCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mococo.delivery.adapters.persistence;
-
-import com.mococo.delivery.domain.model.Category;
-import com.mococo.delivery.domain.repository.CategoryRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface JpaCategoryRepository extends CategoryRepository, JpaRepository<Category, String> {
-}
--- src/main/java/com/mococo/delivery/adapters/persistence/JpaUserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mococo.delivery.adapters.persistence;
-
-import com.mococo.delivery.domain.repository.UserRepository;
-
-public interface JpaUserRepository extends UserRepository {
-}
